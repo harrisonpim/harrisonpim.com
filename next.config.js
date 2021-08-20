1d35af88ac1f884ac59456bdaad4f46b5c1dba74
harrisonpim <harrison.stroma@gmail.com>
2021-08-20 20:52:48 +0100

    add a redirect for the favicon blogpost

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@ module.exports = {
   images: {
     domains: ['images.prismic.io'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/blog/favicon-emoji-magic',
+        destination: '/blog/using-emoji-as-favicons',
+        permanent: true,
+      },
+    ]
+  },
   webpack: (config) => {
     require('./scripts/generate-sitemap')
     return config
