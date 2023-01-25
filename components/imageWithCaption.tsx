7d347c64570b2ce24079d5c3e80bddecb13965cb
harrisonpim <hello@harrisonpim.com>
2023-01-25 23:28:52 +0000

    invert svg images in dark mode

--- components/imageWithCaption.tsx
+++ components/imageWithCaption.tsx
@@ -24,7 +24,10 @@ const ImageWithCaption: FC<Props> = ({ slice }) => {
   return (
     <figure className="mx-auto h-auto w-full py-3 text-center lg:w-4/5">
       <Image
-        className="rounded-sm"
+        // if the image is in dark mode and the type is an svg, it should be inverted
+        className={`rounded-sm ${
+          slice.primary.image.url.includes('.svg') ? 'dark:invert' : ''
+        }`}
         layout="responsive"
         src={slice.primary.image.url}
         alt={slice.primary.image.alt}
