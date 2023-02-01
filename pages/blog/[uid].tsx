ff94a8aca3880d3382de530c85f7dbbc33f2e610
harrisonpim <hello@harrisonpim.com>
2023-02-01 00:22:04 +0000

    create a basic ogp image for blog posts using the title and favicon

--- pages/blog/[uid].tsx
+++ pages/blog/[uid].tsx
@@ -4,10 +4,17 @@ import { RichText, RichTextBlock } from 'prismic-reactjs'
 import BackButton from '../../components/backButton'
 import { Client } from '../../prismic/helpers'
 import { FC } from 'react'
+import Head from 'next/head'
 import Layout from '../../components/layout'
 import SliceZone from '../../components/sliceZone'
 import { formatDate } from '../../components/date'
 import { queryRepeatableDocuments } from '../../prismic/queries'
+
+const baseUrl = {
+  production: 'https://harrisonpim.com',
+  preview: process.env.VERCEL_URL,
+  development: 'http://localhost:3000',
+}[process.env.VERCEL_ENV]
 
 type PostType = {
   post: {
@@ -28,6 +35,9 @@ const Post: FC<PostType> = ({ post }) => {
       description={RichText.asText(post.data.standfirst)}
       favicon={RichText.asText(post.data.favicon)}
     >
+      <Head>
+        <meta property="og:image" content={`${baseUrl}/api/og?title=${RichText.asText(post.data.title)}&emoji=${RichText.asText(post.data.favicon)}`} />
+      </Head>
       <div>
         <BackButton text="back to the blog" href="/blog" />
         <h1 className="leading pt-3 text-3xl font-medium">
