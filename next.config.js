7c7defaa7cdc525438a73610b029944970de1789
harrisonpim <hello@harrisonpim.com>
2024-03-30 14:20:57 +0000

    link to the generated pdf

--- next.config.js
+++ next.config.js
@@ -3,18 +3,6 @@ const withMDX = require('@next/mdx')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  images: {
-    domains: ['images.prismic.io', 'harrisonpim.cdn.prismic.io'],
-    unoptimized: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/cv.pdf',
-        destination: '/api/cv.pdf',
-      },
-    ]
-  },
 }
 
 module.exports = withMDX(nextConfig)
